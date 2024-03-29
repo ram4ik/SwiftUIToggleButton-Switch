4697c4f4e265cd6673e83903e30cc34ba96c20a1
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-29 16:50:38 +0200

    SwiftUI Toggle - Button & Switch

--- SwiftUIToggleButton&Switch/ContentView.swift
+++ SwiftUIToggleButton&Switch/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isWiFiOn = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle("Wi-fi", systemImage: isWiFiOn ? "wifi" : "wifi.slash", isOn: $isWiFiOn)
+                .font(.largeTitle)
+                .tint(isWiFiOn ? .mint : .orange)
+                .toggleStyle(.button)
+                .labelStyle(.titleAndIcon)
+                .contentTransition(.symbolEffect)
         }
         .padding()
     }
