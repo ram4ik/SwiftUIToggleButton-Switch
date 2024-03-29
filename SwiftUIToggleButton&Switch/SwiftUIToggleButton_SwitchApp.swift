302f3af2d7e8fde5d555bca2cabe8411e2d2b432
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-29 16:46:53 +0200

    Initial Commit

---
+++ SwiftUIToggleButton&Switch/SwiftUIToggleButton_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToggleButton_SwitchApp.swift
+//  SwiftUIToggleButton&Switch
+//
+//  Created by Ramill Ibragimov on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToggleButton_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
